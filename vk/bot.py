cd65df012f501d6b1911dc3ab0768ab10c432db8
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2021-04-17 13:44:09 +0700

    Add files via upload

--- vk/bot.py
+++ vk/bot.py
@@ -33,7 +33,7 @@ for event in longpoll.listen():
                 start_cmd.push(botconfig, event, localization, vk, VkKeyboard, VkKeyboardColor, get_random_id)
             if event.message.text == localization['Bot information']['commands']['001'] or event.message.text == localization['Bot information']['commands']['002']:
                 bot_info_cmd.push(botconfig, event, localization, vk, VkKeyboard, VkKeyboardColor, cpuinfo, psutil, os, platform, get_random_id)
-            if event.message.text == localization['Command list']['commands']['001'] or event.message.text == localization['Command list']['commands']['002']:
+            if event.message.text == localization['Commands list']['commands']['001'] or event.message.text == localization['Commands list']['commands']['002']:
                 cmd_list.push(botconfig, event, localization, vk, VkKeyboard, VkKeyboardColor, cpuinfo, psutil, os, platform, get_random_id)
         except Exception as e:
             print(e)
